6cec983a1d14944efe0ad0ed6a174de2f8f5cb1c
JATIN MADAN <jatinj@dewsolutions.in>
2024-06-08 12:29:05 +0530

    final commit

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ const (
 type Task struct{
 	ID int `json:"id"`
 	Title string `json:"title"`
-	Completed bool `json."completed"`
+	Completed bool `json:"completed"`
 }
  var tasks []Task
  var nextID  int 
